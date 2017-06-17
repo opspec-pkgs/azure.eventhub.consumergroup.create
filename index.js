bc23ae66f72b809dbccc3598037db0e6ab55c811
chris dostert <chris.dostert@nintex.com>
2017-06-17 13:50:55 -0700

    chore) update error handling

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 const msRestAzure = require('ms-rest-azure');
 const AzureArmEventhub = require('azure-arm-eventhub');
+const process = require('process');
 
 const login = async () => {
+    console.log('logging in');
+
     const loginType = process.env.loginType;
     const loginId = process.env.loginId;
     const loginSecret = process.env.loginSecret;
-    const loginOpts = { domain: process.env.loginTenantId};
+    const loginOpts = {domain: process.env.loginTenantId};
 
+    let response;
     if (loginType === 'sp') {
         // https://github.com/Azure/azure-sdk-for-node/blob/master/runtime/ms-rest-azure/index.d.ts#L397
-        return msRestAzure.loginWithServicePrincipalSecret(loginId, loginSecret, loginOpts);
+        response = await msRestAzure.loginWithServicePrincipalSecret(loginId, loginSecret, loginOpts);
     } else {
         // https://github.com/Azure/azure-sdk-for-node/blob/master/runtime/ms-rest-azure/index.d.ts#L356
-        return msRestAzure.loginWithUsernamePassword(loginId, loginSecret, loginOpts);
+        response = await msRestAzure.loginWithUsernamePassword(loginId, loginSecret, loginOpts);
     }
+
+    console.log('login successful');
+
+    return response;
 };
 
-console.log('logging in');
-credentials = login().then(credentials => {
-    console.log('login successful');
+const createOrUpdate = async (credentials) => {
+    console.log('creating/updating consumer group');
 
     const armEventHub = new AzureArmEventhub(credentials, process.env.subscriptionId);
 
     // see https://github.com/Azure/azure-sdk-for-node/blob/master/lib/services/eventHubManagement/lib/operations/consumerGroups.js#L999
-    console.log('creating/updating consumer group');
-    armEventHub.consumerGroups.createOrUpdate(
+    await armEventHub.consumerGroups.createOrUpdate(
         process.env.resourceGroup,
         process.env.namespace,
         process.env.eventHub,
         process.env.name,
-        error => {
-            if (error) {
-                throw error;
-            }
-            console.log('creating/updating consumer group successful');
-        }
     );
+
+    console.log('creating/updating consumer group successful');
+};
+
+login().then(createOrUpdate).catch(error => {
+    console.log(error);
+    process.exit(1)
 });
